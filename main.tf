4658cf5df575b0a39971306f500db10960a144cf
Ivan Wijaya <ivan.wijaya@mii.co.id>
2023-04-10 06:02:23 +0700

    update

---
+++ main.tf
@@ -0,0 +1,49 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.3.0"
+    }
+  }
+}
+// Configure the Google Cloud provider
+provider "google" {
+  credentials = file("credentials_file.json")
+  project     = "devops-test-361018"
+  region      = "asia-southeast2"
+}
+
+# resource "google_container_cluster" "primary" {
+#   name             = "ws-sby-cluster"
+#   location         = "asia-southeast2"
+#   enable_autopilot = true
+#   vertical_pod_autoscaling {
+#     enabled = true
+#   }
+# }
+
+resource "google_compute_instance" "main" {
+  name         = "richie-test2"
+  machine_type = "e2-micro"
+  zone         = "asia-southeast2-a"
+
+  tags         = ["rdp","test-richie", "http", "https"]
+
+  boot_disk {
+    initialize_params {
+      image = "centos-cloud/centos-7-v20230306"
+      size  = 50
+      type  = "pd-balanced"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+        // Ephemeral public IP
+    }
+  }
+
+}
+
